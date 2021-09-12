503d30999b4c3b7fbb831a63d5e077d105ea6eab
louidaking123 <louidaking123@protonmail.com>
2021-09-12 06:00:35 +0300

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -18,11 +18,10 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: apple, Ubuntu, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: gray;
+  font-size: medium;
+  font-style: italic;
 }
 </style>
